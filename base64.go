ea5696606fc69e82d760947feac9a84136289f2c
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-09-23 13:00:38 -0300

    Some fixes

--- base64.go
+++ base64.go
@@ -11,43 +11,99 @@ import (
 )
 
 var (
-	col = flag.Int("c", 64, "Columns")
+	col = flag.Int("w", 64, "Wrap lines after N columns")
 	dec = flag.Bool("d", false, "Decode instead of Encode")
 	pad = flag.Bool("n", false, "No padding")
 )
 
 func main() {
 	flag.Parse()
-	if *dec == false && *pad == false {
-		data, _ := ioutil.ReadAll(os.Stdin)
-		b := strings.TrimSuffix(string(data), "\r\n")
-		b = strings.TrimSuffix(b, "\n")
-		sEnc := b64.StdEncoding.EncodeToString([]byte(b))
-		for _, chunk := range split(sEnc, *col) {
-			fmt.Println(chunk)
-		}
-	} else if *dec && *pad == false {
-		data, _ := ioutil.ReadAll(os.Stdin)
-		b := strings.TrimSuffix(string(data), "\r\n")
-		b = strings.TrimSuffix(b, "\n")
-		sDec, _ := b64.StdEncoding.DecodeString(b)
-		os.Stdout.Write(sDec)
-	}
 
-	if *dec == false && *pad == true {
-		data, _ := ioutil.ReadAll(os.Stdin)
-		b := strings.TrimSuffix(string(data), "\r\n")
-		b = strings.TrimSuffix(b, "\n")
-		sEnc := b64.StdEncoding.WithPadding(-1).EncodeToString([]byte(b))
-		for _, chunk := range split(sEnc, *col) {
-			fmt.Println(chunk)
+	if *col == 0 && len(flag.Args()) > 0 {
+		inputFile := flag.Arg(0)
+
+		data, err := ioutil.ReadFile(inputFile)
+		if err != nil {
+			fmt.Println("Error reading the file:", err)
+			os.Exit(1)
 		}
-	} else if *dec && *pad == true {
-		data, _ := ioutil.ReadAll(os.Stdin)
-		b := strings.TrimSuffix(string(data), "\r\n")
-		b = strings.TrimSuffix(b, "\n")
-		sDec, _ := b64.StdEncoding.WithPadding(-1).DecodeString(b)
-		os.Stdout.Write(sDec)
+
+		inputData := string(data)
+		inputData = strings.TrimSuffix(inputData, "\r\n")
+		inputData = strings.TrimSuffix(inputData, "\n")
+
+		if *dec == false && *pad == false {
+			sEnc := b64.StdEncoding.EncodeToString([]byte(inputData))
+			fmt.Println(sEnc)
+		} else if *dec && *pad == false {
+			sDec, _ := b64.StdEncoding.DecodeString(inputData)
+			os.Stdout.Write(sDec)
+		}
+
+		if *dec == false && *pad == true {
+			sEnc := b64.StdEncoding.WithPadding(-1).EncodeToString([]byte(inputData))
+			fmt.Println(sEnc)
+		} else if *dec && *pad == true {
+			sDec, _ := b64.StdEncoding.WithPadding(-1).DecodeString(inputData)
+			os.Stdout.Write(sDec)
+		}
+	} else {
+		var inputData string
+
+		if len(flag.Args()) == 0 {
+			data, _ := ioutil.ReadAll(os.Stdin)
+			inputData = string(data)
+		} else {
+			inputFile := flag.Arg(0)
+
+			data, err := ioutil.ReadFile(inputFile)
+			if err != nil {
+				fmt.Println("Error reading the file:", err)
+				os.Exit(1)
+			}
+			inputData = string(data)
+		}
+
+		inputData = strings.TrimSuffix(inputData, "\r\n")
+		inputData = strings.TrimSuffix(inputData, "\n")
+
+		if *col != 0 {
+			if *dec == false && *pad == false {
+				sEnc := b64.StdEncoding.EncodeToString([]byte(inputData))
+				for _, chunk := range split(sEnc, *col) {
+					fmt.Println(chunk)
+				}
+			} else if *dec && *pad == false {
+				sDec, _ := b64.StdEncoding.DecodeString(inputData)
+				os.Stdout.Write(sDec)
+			}
+
+			if *dec == false && *pad == true {
+				sEnc := b64.StdEncoding.WithPadding(-1).EncodeToString([]byte(inputData))
+				for _, chunk := range split(sEnc, *col) {
+					fmt.Println(chunk)
+				}
+			} else if *dec && *pad == true {
+				sDec, _ := b64.StdEncoding.WithPadding(-1).DecodeString(inputData)
+				os.Stdout.Write(sDec)
+			}
+		} else {
+			if *dec == false && *pad == false {
+				sEnc := b64.StdEncoding.EncodeToString([]byte(inputData))
+				fmt.Println(sEnc)
+			} else if *dec && *pad == false {
+				sDec, _ := b64.StdEncoding.DecodeString(inputData)
+				os.Stdout.Write(sDec)
+			}
+
+			if *dec == false && *pad == true {
+				sEnc := b64.StdEncoding.WithPadding(-1).EncodeToString([]byte(inputData))
+				fmt.Println(sEnc)
+			} else if *dec && *pad == true {
+				sDec, _ := b64.StdEncoding.WithPadding(-1).DecodeString(inputData)
+				os.Stdout.Write(sDec)
+			}
+		}
 	}
 }
 
@@ -58,7 +114,6 @@ func split(s string, size int) []string {
 			size = len(s)
 		}
 		ss, s = append(ss, s[:size]), s[size:]
-
 	}
 	return ss
 }
